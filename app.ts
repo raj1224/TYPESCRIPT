2caa4150b26c29f11d812de5b5c34fb0470eebe1
raj <imraaj10262gmail.comclear>
2025-06-21 21:54:34 +0530

    finished

--- app.ts
+++ app.ts
@@ -229,3 +229,260 @@
 // }
 // let b1 = new metalbottlemaker('chilton');
 // b1.getvalue
+
+// PROTECTED
+
+// class BottleMaker{
+//     protected name="milton";
+// }
+// class MetalBottleMaker extends BottleMaker{
+//     public material = "metal";
+//     changeName(){
+//         this.name="some other name";
+//     }
+// }
+
+// let b1 = new MetalBottleMaker();
+// b1.name="some more names of bottle";
+
+// OPTIONAL PROPERTIES
+// class User{
+//     constructor(public readonly name:string){
+
+//     }
+//     changeName (){
+//         this.name ="hellow"
+//     }
+// }
+// let u1 = new User('harsh');
+
+// CONSTRUCTOR
+//access modifiers (public,private,protected)
+// readonly properties
+// optional properties
+// parameter properties
+// getters and setters
+// static memberes
+// abstract classes and methods
+
+
+// class User{
+//     constructor(public name:string,public age:number,public gender?:string){
+
+//     }
+// }
+// let u1 =new User('raj',21,"male")
+
+// getteres and setters
+
+// class User{
+//     constructor(public _name:string,public age:number,public gender?:string){
+
+//     }
+//     get name(){
+//         return this._name;
+//     }
+//     set name(value:string){
+//         this._name=value;
+//     }//_name is liye likha h kyuki fir hum apne methods ka naam get or set likh skte h
+// }
+// let u1 =new User('raj',21,"male")
+// u1.name 
+// u1.name="raju"
+
+
+// STATIC MEMORY
+// class Shery{
+//     static version =1.0;
+//     static getRandomNumber(){
+//         return Math.random();
+//     }
+// }
+
+
+// ABSTRACT CLASSES
+// class paytm{
+//     constructor(protected amount:number,protected account :number){}
+//     ispaymentvalid(amount:number){
+//         return this.amount>0;
+//     }
+// }
+
+// class cookingessentials{
+//     constructor(protected gas:number,public gaskanaam:string){}
+
+// }
+// class sabji extends cookingessentials{
+
+// }
+// class cake extends cookingessentials{
+
+// }
+
+
+// FUNCTIONS
+// function types
+// optional and default parameters
+// rest parameteres
+// overloads
+
+
+// function abcdef(name:string,age:number,cb:(arg:string)=>void){
+//     cb('hey')
+// }
+
+// abcdef("raj",21,(arg:string)=>{
+//     console.log(arg);
+    
+// })
+
+// OPTIONAL AND DEFAULT PARAMETERS
+// function abcdef(name:string,age:number,gender:string="not to be discloses"){
+//     console.log(name,age,gender);
+    
+// }
+// // function abcdef(name:string,age:number,gender?:string){
+
+// // }
+
+// abcdef("raj",21,'male')
+// abcdef("raju",21,)
+
+
+// REST PARAMETERS
+
+// ...  rest/spread
+
+// function sum(...arr:number[]){
+//     console.log(arr[3]);   
+// }
+// sum(1,23,4,56,78,90,1,2)
+
+
+// FUNCTION OVERLOADING
+
+// function abcd (a:string):void;
+// function abcd(a:string,b:number):number;
+
+// function abcd(a:any,b?:any){
+//     if(typeof a==="string"&& typeof b==="number"){
+//         return 123
+//     }
+//     if(typeof a==="string"&& typeof b===undefined){
+//         console.log("hey");
+//     }
+//     else throw new Error('something went wrong')
+// }
+
+// abcd('hey')
+// abcd('hey',12);
+
+// GENERICS
+// generics functions
+// generic interfaces
+// generics classes
+
+// function log<T>(val:T){
+//     console.log(val)
+
+// }
+// log<string>('hey');
+
+// INTERFACES
+// interface Halua<T>{
+//     name:string;
+//     age:number;
+//     key:T;
+// }
+// function abcd(obj:Halua<string>){
+
+// }
+// abcd({name:'foo',age:25,key:"agafjaio"})
+
+// CLASSES
+
+// class BottleMaker<T>{
+//     constructor (public key:T){}
+// }
+// let b1 = new BottleMaker<string>("hey")
+// let b2 = new BottleMaker<number>(12)
+// console.log(b1,b2);
+
+
+ // Another Example
+
+//  "string"->string (X) but it is STRING LITERAL (T) 
+
+//  function abcd <T>(a:T,b:T):T{
+//     // return <T>"hey";
+//     // return "hey" as T;
+//     if(typeof a ==="string"){
+//         a.
+//     }
+//  }
+//  abcd<string>("hey","hello");
+
+// MODULES
+
+// exporting and importing modules
+// default exports
+
+// import {getDetails,addPayments}from "./payment";
+// addPayments(12)
+
+
+// import Payment from './payment';
+
+// let a = new Payment(12);
+// let B= new Payment(122);
+// console.log(a,B);
+
+
+// TYPE ASSERTION
+// type casting
+// non-null assertion operator
+
+
+// let a:any=12;
+// (<number>a).
+
+// let a =Number('12');
+// console.log(a);
+// console.log(typeof a);
+
+
+// non-null assertion operator
+// let a:null | undefined | string;
+// a="hey";
+// a.
+
+// TYPE GUARDS AND TYPESCRIPT UTILITY TYPES
+// using typeof and instanceof 
+// partial , required ,readonly
+
+// type guard ->type narrowing
+// function abcd(arg:string | number){
+//     if(typeof arg ==="number"){
+//         return "number";
+//     }
+//     else if(typeof arg ==="string"){
+//         return "string";
+//     }
+//     else{
+//         throw new Error('pagal how gaya hai fraaands')
+//     }
+// }
+// console.log(abcd(12));
+// console.log(abcd("helloew"));
+
+// 
+
+function switchOffKaro(device:tvkaremote | carkaremote){
+    if(device instanceof tvkaremote){
+        device.switchOffKaro();
+    }
+    else if(device instanceof carkaremote){
+        device.switchOffKaro();
+    }
+}
+switchOffKaro();
---
+++ payment.ts
@@ -0,0 +1,13 @@
+// export function addPayment(val:number){
+//     console.log(val);
+    
+// }
+// export function getDetails(){
+
+// }
+
+export default class Payment{
+    constructor(public price:number){
+
+    }
+}
